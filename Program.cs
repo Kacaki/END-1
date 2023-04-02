5bb4ef7150fca13ed0758b4c1318b1f72025af0e
Roman <ro@bk.ru>
2023-04-02 15:01:15 +0500

    добавил решение программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] arr1 = new string[8] {"12345", "98", "Слово", "Дело", "Земля", "Ум", "67", "333"};
+string[] arr2 = new string[arr1.Length];
+
+
+
+void SecondArrayStatus(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+PrintArray(arr1);
+SecondArrayStatus(arr1, arr2);
+PrintArray(arr2);
